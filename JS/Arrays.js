07f4d18cd2070797e78a94331e61c5cb60e477a3
mayurnasre <mayurnasre2020@gmail.com>
2024-07-26 15:56:17 +0530

    first commit files and folder added to first commit

---
+++ JS/Arrays.js
@@ -0,0 +1,8 @@
+let arr=["jjje",3434,true];
+let arr1=new Array(5);
+console.log("5 ki length "+arr1.length);
+console.log("originalki length "+arr.length);
+
+let arr2=new Array(2,4);
+console.log("2 para ki length "+arr2.length);
+
